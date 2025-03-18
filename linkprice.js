12b05e2a5105085eabc656fb87af7b99f31ab1c7
multicore-it <78068633+multicore-it@users.noreply.github.com>
2025-03-19 00:19:26 +0900

    Update linkprice.js

--- linkprice.js
+++ linkprice.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let targetElement = document.getElementById("article-view") || document.getElementById("entry-view"); // 원하는 위치 선택
+    //let targetElement = document.getElementById("article-view") || document.getElementById("entry-view"); // 원하는 위치 선택
+	let targetElement = document.getElementById("linkprice-ad"); // 원하는 위치 선택
 
     if (targetElement) {
         let customHTML = `
@@ -26,15 +27,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // 특정 위치에 추가
         // targetElement.insertAdjacentHTML("afterbegin", customHTML); // 맨 위에 추가
-        // targetElement.insertAdjacentHTML("beforeend", customHTML); // 맨 아래에 추가
+        targetElement.insertAdjacentHTML("beforeend", customHTML); // 맨 아래에 추가
         // targetElement.insertAdjacentHTML("beforebegin", customHTML); // 요소 이전에 추가
         // targetElement.insertAdjacentHTML("afterend", customHTML); // 요소 이후에 추가
 		        // 요소에 따라 삽입 위치 결정
-        if (targetElement.id === "article-view") { //PC
-            targetElement.insertAdjacentHTML("beforeend", customHTML); // 내부 맨 아래 삽입
-        } else if (targetElement.id === "entry-view") { //모바일
-            targetElement.insertAdjacentHTML("afterbegin", customHTML); // 요소 바로 앞에 삽입
-        }
+        //if (targetElement.id === "article-view") { //PC
+        //    targetElement.insertAdjacentHTML("beforeend", customHTML); // 내부 맨 아래 삽입
+        //} else if (targetElement.id === "entry-view") { //모바일
+        //    targetElement.insertAdjacentHTML("afterbegin", customHTML); // 요소 바로 앞에 삽입
+        //}
     } else {
         console.log("❌ 광고를 삽입할 위치를 찾을 수 없음.");
     }
