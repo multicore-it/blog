7818b226a64aefcae8f1a9fb94cc1ee3498b535f
multicore-it <78068633+multicore-it@users.noreply.github.com>
2025-03-19 00:08:25 +0900

    Update linkprice.js

--- linkprice.js
+++ linkprice.js
@@ -26,9 +26,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // 특정 위치에 추가
         // targetElement.insertAdjacentHTML("afterbegin", customHTML); // 맨 위에 추가
-        targetElement.insertAdjacentHTML("beforeend", customHTML); // 맨 아래에 추가
+        // targetElement.insertAdjacentHTML("beforeend", customHTML); // 맨 아래에 추가
         // targetElement.insertAdjacentHTML("beforebegin", customHTML); // 요소 이전에 추가
         // targetElement.insertAdjacentHTML("afterend", customHTML); // 요소 이후에 추가
+		        // 요소에 따라 삽입 위치 결정
+        if (targetElement.id === "article-view") { //PC
+            targetElement.insertAdjacentHTML("beforeend", customHTML); // 내부 맨 아래 삽입
+        } else if (targetElement.id === "entry-view") { //모바일
+            targetElement.insertAdjacentHTML("afterbegin", customHTML); // 요소 바로 앞에 삽입
+        }
     } else {
         console.log("❌ 광고를 삽입할 위치를 찾을 수 없음.");
     }
